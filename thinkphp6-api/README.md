90120a6599426f35becba3e89867cf64cfc3a4c3
hainiugege <75881931+hainiugege@users.noreply.github.com>
2024-01-06 15:49:50 +0800

    Add files via upload

---
+++ thinkphp6-api/README.md
@@ -0,0 +1,2 @@
+# 海牛自定义物流接口
+The ThinkPHP6 API Package
---
+++ thinkphp6-api/src/config/wlConfig.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by hainiu.
+ * User: hainiu
+ * Date: 2023/12/08
+ * Time: 22:50
+ */
+return[
+    'url'           => 'https://www.haiousudi.com/hwc_api/', // API URL
+];
